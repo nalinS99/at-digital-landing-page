cf8441e7987fed51888b365d18cdee99cff47a0d
nalin <rmnsbandara@std.appsc.sab.ac.lk>
2025-02-01 11:35:07 +0530

    initial commit

---
+++ at-digital/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+
+  return (
+    <>
+     <div className="min-h-screen flex items-center justify-center bg-gray-200">
+      <h1 className="text-9xl font-bold text-blue-500">at  digital !</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
